e091494353fc6379dbacdf6e9ad879d156fe4d95
strange-life <strange.hao@gmail.com>
2020-10-24 20:47:42 +0800

    progress through story

--- lib/main.dart
+++ lib/main.dart
@@ -55,8 +55,9 @@ class _StoryPageState extends State<StoryPage> {
                   flex: 2,
                   child: FlatButton(
                     onPressed: () {
-                      //Choice 1 made by user.
-                      //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                      setState(() {
+                        _storyBrain.nextStory(1);
+                      });
                     },
                     color: Colors.red,
                     child: Text(
@@ -76,8 +77,9 @@ class _StoryPageState extends State<StoryPage> {
                   //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                   child: FlatButton(
                     onPressed: () {
-                      //Choice 2 made by user.
-                      //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      setState(() {
+                        _storyBrain.nextStory(2);
+                      });
                     },
                     color: Colors.blue,
                     child: Text(
@@ -96,7 +98,5 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
