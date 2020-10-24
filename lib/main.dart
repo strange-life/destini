3957adee8b96deb57a2156c29c37b03bc5d286d3
strange-life <strange.hao@gmail.com>
2020-10-24 20:56:23 +0800

    hidden choice2 button when choice2 is empty

--- lib/main.dart
+++ lib/main.dart
@@ -71,21 +71,22 @@ class _StoryPageState extends State<StoryPage> {
                 SizedBox(
                   height: 20.0,
                 ),
-                Expanded(
-                  flex: 2,
-                  //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                  //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                  child: FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        _storyBrain.nextStory(2);
-                      });
-                    },
-                    color: Colors.blue,
-                    child: Text(
-                      _storyBrain.getChoice2(),
-                      style: TextStyle(
-                        fontSize: 20.0,
+                Visibility(
+                  visible: _storyBrain.buttonShouldBeVisible(),
+                  child: Expanded(
+                    flex: 2,
+                    child: FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          _storyBrain.nextStory(2);
+                        });
+                      },
+                      color: Colors.blue,
+                      child: Text(
+                        _storyBrain.getChoice2(),
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
                       ),
                     ),
                   ),
@@ -98,5 +99,3 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -66,6 +66,8 @@ class StoryBrain {
 
     print(_storyNumber);
   }
-}
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    return _storyNumber < 3;
+  }
+}
